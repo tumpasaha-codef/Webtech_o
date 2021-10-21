ac916a1b3cd2e4964b129cb9ba6ac3e0d8622c29
Tumpa saha <tumpasah715@gmail.com>
2021-10-21 10:59:32 +0600

    “Digital-wallet”

---
+++ Digital-wallet/tansaction history.php
@@ -0,0 +1,35 @@
+<DOCTYPE html>
+	<html>
+	<head>
+		<meta charset = "utf-8">
+		<title>Digital Wallet</title>
+	</head>
+	<body>
+		<h1>Digital Wallet</h1>
+		
+		 <p> 1.<a href="Home.html">Home</a> 2.  <a href="Add-Beneficiary.php">Add-Beneficiary</a> 3.  <a href= "Transaction-Hidtory.php">  Transaction-Hidtory</a> </p> 
+
+	    <br><br>
+		
+		<form action=" " method="POST">
+		
+		<label for="start">From:*</label>
+
+       <input type="date" id="start" name="trip-start"
+            min="1998-01-01" max="2040-12-31">
+			
+			<label for="start">To:*</label>
+
+       <input type="date" id="start" name="trip-start"
+            min="1998-01-01" max="2040-12-31">
+			
+			<input type="submit" name="Search" value="Search">
+			
+			<br> <br>
+         
+        <label><b>Fund Transfer:</b></label>
+        
+	 </form>
+
+
+	</body>
